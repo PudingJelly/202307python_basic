bf6746f03574c9d32bd7405fd8b576a0dc529b8a
changju <changju7857@gmail.com>
2023-08-08 18:16:57 +0900

    # 3. set quiz

---
+++ data_type/set/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+new_list = []
+
+# for num in li:
+#     if num not in li2:
+#         new_list.append(num)
+
+# for num in li2:
+#     if num not in li:
+#         new_list.append(num)
+
+# print(new_list)
